81e1c4d3bfd67d91ee068a9ce988527ed387ce4b
WYQ <wyqpadding@gmail.com>
2019-04-03 18:30:52 +0800

    增加拦截器

--- src/core/Fugui.js
+++ src/core/Fugui.js
@@ -1,20 +1,47 @@
-import {forEach} from './uitls'
+import {forEach} from './utils'
+import {InterceptorManager} from './Interceptor'
+import mergeConfig from './merge'
 
-function Fugui(bridge)  {
+function Fugui(bridge, config)  {
     this.bridge = bridge;
+    this.defaults = config
+    console.log(config);
+    this.interceptors = {
+        request: new InterceptorManager(),
+        response: new InterceptorManager()
+    };
 }
 
-Fugui.prototype.request = function request(configure) {
+Fugui.prototype.request = function request(config) {
     var bridge = this.bridge;
+    if (typeof config === 'string') {
+        config = arguments[1] || {};
+        config.url = arguments[0];
+      } else {
+        config = config || {};
+      }
+    
+    config = mergeConfig(this.defaults, config);
+    config.method = config.method ? config.method.toLowerCase() : 'get';
+    var interceptors = this.interceptors
     var promise = new Promise(function(resolve, reject) {
-        var debug = configure.fg_debug
+        interceptors.request.forEach(function requestInterceptors(interceptor) {
+            interceptor.fulfilled(config);
+        });
+        var debug = config.params.fg_debug
         if (debug == true) {
             setTimeout(function(){
+                interceptors.response.forEach(function responseInterceptors(interceptor) {
+                    interceptor.fulfilled("您的测试预期返回成功，返回成功");
+                });
                 resolve("您的测试预期返回成功，返回成功");
             }, 1000);
         } else {
-            bridge.call("onAjaxRequest", configure, function(value) {
+            bridge.call("onAjaxRequest", config, function(value) {
                 var json = JSON.parse(value);
+                interceptors.response.forEach(function responseInterceptors(interceptor) {
+                    interceptor.fulfilled(json.value);
+                });
                 var success = json.success;
                 if (success == true) {
                     resolve(json.value);
@@ -37,8 +64,4 @@ forEach(['get','delete','head','put','post'], function (method) {
     };
 });
 
-// module.exports = Fugui;
 export default Fugui;
-// export {
-//     Fugui
-// }
--- src/fugui.js
+++ src/fugui.js
@@ -1,14 +1,18 @@
 import dsbridge from 'dsbridge'
 import Fugui from './core/Fugui'
-import {bind, extend} from './core/uitls'
+import {bind, extend} from './core/utils'
 
-function createInstance(bridge) {
-    var context = new Fugui(bridge);
+function createInstance(bridge, config) {
+    var context = new Fugui(bridge, config);
     var instance = bind(Fugui.prototype.request, context);
     extend(instance, Fugui.prototype, context);
     extend(instance, context);
     return instance;
 }
 // Create the default instance to be exported
-var fugui = createInstance(dsbridge);
+
+var fugui = createInstance(dsbridge, {});
+fugui.create = function create(instanceConfig) {
+    return createInstance(dsbridge, instanceConfig);
+};
 export default fugui;
